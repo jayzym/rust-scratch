52134f6eb12fb2cb26aec26c83b34ed2994bc62a
Kevin Stoner <kstoner@inovonics.com>
2020-03-11 00:39:14 -0600

    removing unnecessary files and adding .gitignore

--- rectangles/target/debug/deps/rectangles-4faae5285aa37e05.d
+++
@@ -1,5 +0,0 @@
-/Users/jared/git/rust-scratch/rectangles/target/debug/deps/rectangles-4faae5285aa37e05: src/main.rs
-
-/Users/jared/git/rust-scratch/rectangles/target/debug/deps/rectangles-4faae5285aa37e05.d: src/main.rs
-
-src/main.rs:
